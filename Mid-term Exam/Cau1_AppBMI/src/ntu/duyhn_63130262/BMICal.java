ae9ae3e0d5e524f59800ebd4274ff7361f2f8e1a
UnknownLvl4 <hongocduyyu@gmail.com>
2024-03-25 09:29:53 +0700

    Khai báo các đối tượng
    Tạo cửa sổ đồ họa; đối tượng nhập chiều cao và cân nặng; số BMI đã được tính ra; tình trạng của cơ thể sau khi tính được số BMI.

--- Mid-term Exam/Cau1_AppBMI/src/ntu/duyhn_63130262/BMICal.java
+++ Mid-term Exam/Cau1_AppBMI/src/ntu/duyhn_63130262/BMICal.java
@@ -1,10 +1,20 @@
 package ntu.duyhn_63130262;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class BMICal {
+	JFrame f;
+	JTextField Height;
+	JTextField Weight;
+	JLabel BMINum;
+	JLabel Status;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
