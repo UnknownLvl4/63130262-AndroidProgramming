4eb7fd660ec72afc77d903c0d0ac778cf501e526
UnknownLvl4 <hongocduyyu@gmail.com>
2024-03-25 22:15:02 +0700

    AppStopwatch project create

---
+++ Mid-term Exam/Cau2_AppStopwatch/app/src/main/java/ntu/duyhn_63130262/cau2_appstopwatch/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.duyhn_63130262.cau2_appstopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Mid-term Exam/Cau2_AppStopwatch/app/src/test/java/ntu/duyhn_63130262/cau2_appstopwatch/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ntu.duyhn_63130262.cau2_appstopwatch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
