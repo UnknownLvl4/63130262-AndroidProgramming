c0b66d9a2a808048fcce01e4cf65d61e58b49407
UnknownLvl4 <hongocduyyu@gmail.com>
2024-03-25 19:30:06 +0700

    Cập nhật code và file màn hình chạy App BMI

--- Mid-term Exam/Cau1_AppBMI/src/ntu/duyhn_63130262/BMICal.java
+++ Mid-term Exam/Cau1_AppBMI/src/ntu/duyhn_63130262/BMICal.java
@@ -41,7 +41,7 @@ public class BMICal {
 		f = new JFrame();
 		f.setTitle("BMI Calculator");
 		f.setBounds(550, 250, 300, 200);
-		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.getContentPane().setLayout(null);
 
 		JLabel hLabel = new JLabel("Chiều cao (m):");
@@ -73,27 +73,27 @@ public class BMICal {
 				double h = Double.parseDouble(Height.getText());
 				double w = Double.parseDouble(Weight.getText());
 				double bmi = w / (h * h);
-				BMINum = new JLabel("");
-				BMINum.setBounds(10, 120, 300, 25);
-				f.getContentPane().add(BMINum);
 				BMINum.setText(String.format("Chỉ số BMI của bạn là: %.2f", bmi));
-				
-				Status = new JLabel("");
-				Status.setBounds(10, 137, 300, 25);
-				f.getContentPane().add(Status);
-				if(bmi<18.5) {
+				if (bmi < 18.5) {
 					Status.setText("Bạn đang bị gầy!");
-				}
-				else if (bmi<24.9) {
+				} 
+				else if (bmi < 24.9) {
 					Status.setText("Bạn đang ở trong tình trạng cân đối!");
-				}
+				} 
 				else if (bmi < 29.9) {
 					Status.setText("Bạn đang bị thừa cân!");
-				}
+				} 
 				else {
 					Status.setText("Bạn đang bị béo phì!");
 				}
 			}
 		});
+		BMINum = new JLabel("");
+		BMINum.setBounds(10, 120, 300, 25);
+		f.getContentPane().add(BMINum);
+		
+		Status = new JLabel("");
+		Status.setBounds(10, 137, 300, 25);
+		f.getContentPane().add(Status);
 	}
 }
