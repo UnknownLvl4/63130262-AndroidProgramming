97e308854aef81c585e49c6fb2cb5e37a923611d
UnknownLvl4 <hongocduyyu@gmail.com>
2024-03-25 23:48:13 +0700

    Khai báo các đối tượng và điều khiển
    Tạo các đối tượng gắn với điều khiển tương ứng; tạo hàm tìm điều khiển.

--- Mid-term Exam/Cau2_AppStopwatch/app/src/main/java/ntu/duyhn_63130262/cau2_appstopwatch/MainActivity.java
+++ Mid-term Exam/Cau2_AppStopwatch/app/src/main/java/ntu/duyhn_63130262/cau2_appstopwatch/MainActivity.java
@@ -3,12 +3,26 @@ package ntu.duyhn_63130262.cau2_appstopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView TextViewtimer;
+    Button nutStart, nutPause, nutReset;
+    int sec = 0;
+    boolean run;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FindControl();
+    }
+
+    void FindControl() {
+        TextViewtimer = (TextView) findViewById(R.id.timer);
+        nutStart = (Button) findViewById(R.id.btnStart);
+        nutPause = (Button) findViewById(R.id.btnPause);
+        nutReset = (Button) findViewById(R.id.btnReset);
     }
 }
