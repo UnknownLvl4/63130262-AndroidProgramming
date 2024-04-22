58e85b1fe5df55afc51fa33651de0342fbca65e2
UnknownLvl4 <hongocduyyu@gmail.com>
2024-04-23 06:18:35 +0700

    RecyclerView Project - Display list of objects

--- UsingRecyclerView/app/src/main/java/duy/hn/usingrecyclerview/MainActivity.java
+++ UsingRecyclerView/app/src/main/java/duy/hn/usingrecyclerview/MainActivity.java
@@ -1,14 +1,40 @@
 package duy.hn.usingrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape>recyclerViewDatas;
+    RecyclerView recyclerViewLandscape;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //3
+        recyclerViewDatas=getDataForRecyclerView();
+        //4
+        recyclerViewLandscape=findViewById(R.id.recyclerLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear= new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter=new LandScapeAdapter(this, recyclerViewDatas);
+        //7
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
+    }
+
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu=new ArrayList<LandScape>();
+        LandScape landScape1=new LandScape("dog", "Chú Chó");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("Panda","Gấu Trúc"));
+        dsDuLieu.add(new LandScape("Zebra","Ngựa vằn"));
+        return dsDuLieu;
     }
 }
